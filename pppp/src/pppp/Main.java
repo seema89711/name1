05f2b7e36dcf24198d9872733575e62877963e12
admin <admin@DESKTOP-M8MBDHK>
2024-12-28 16:57:02 +0530

    new

--- pppp/src/pppp/Main.java
+++ pppp/src/pppp/Main.java
@@ -4,8 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world");
-System.out.println("hello ");
-System.out.println("java devloper");
 	}
 
 }
